c9207000a68b1d75d8bc1585269672a33ae4358c
Saloni Sharma <saloni@pragyaam.in>
2019-09-12 14:19:26 +0530

    mapping dispatch to props

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addCharacterById } from '../actions';
 
 class CharacterList extends Component {
     render() {
+        console.log('this.props', this.props)
         return (
             <div>
                 <h4>Characters</h4>
                 <ul>
-                {
-                this.props.characters.map(character => {
-                    return (
-                        <li key={character.id}>
-                        <div>
-                            {character.name}
-                        </div>
-                        </li>
-                    )
-                })
-                }
+                    {
+                        this.props.characters.map(character => {
+                            return (
+                                <li key={character.id}>
+                                    <div>
+                                        {character.name}
+                                    </div>
+                                    <div onClick={() => this.props.addCharacterById(character.id)}>+</div>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </div>
         )
@@ -30,4 +34,8 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps, null)(CharacterList);
+// function mapDispatchToProps(dispatch) {
+//     return bindActionCreators({ addCharacterById }, dispatch)
+// }
+
+export default connect(mapStateToProps, {addCharacterById})(CharacterList);
