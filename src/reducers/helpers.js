1f085c1de9db29fe16f8943b33f91081d614192d
muthukumar <partner.muthukumar.k@philips.com>
2020-01-21 16:05:39 +0530

    deleted files

--- src/reducers/helpers.js
+++
@@ -1,7 +0,0 @@
-import characters_json from '../data/characters.json';
-
-export function createCharacter(id) {
-    let characters = characters_json.find(c => c.id === id);
-    return characters;
-
-}
